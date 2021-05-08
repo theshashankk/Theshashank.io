e3f989a4cd63b79febf0beda93aa74092e370575
theshashankk <78837537+theshashankk@users.noreply.github.com>
2021-05-08 06:00:41 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+  tgUsername: "xD_Shaahank",
+  githubUsername: "Theshashankk",
+  websiteName: "WhiteDevilX",
+  heroHead: "Hlo, Bruh!",
+  heroText: "Hey I'm shashank Form jharkhand",
+  facebookUsername: "TheshashankSharma",
+  instaUsername: "Real_Sharmashashank",
+  typedArray : ["A Noob Developer 👨🏻‍💻","Slow Learner 🤣","Song Lover 🎶"," FF Player 🤓", "Always using TG😅"]
+};
